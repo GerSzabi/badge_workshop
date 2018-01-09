82afd6c90dd537f209789ae355798267bcf571b9
Szabolcs Gergye <szabolcs.gergye@attrecto.com>
2018-01-09 10:15:13 +0100

    hello world

--- app.js
+++ app.js
@@ -6,4 +6,27 @@ const index = require('./routes/index');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use('/api', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    console.log(err, err.message, err.stack);
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
+
 module.exports = app;
